ef7a059eeadcef3a104d98325d98372742c5a1d6
anthonymoisan <anthonymoisan@yahoo.fr>
2026-01-15 12:37:59 +0100

    Mise à jour Politique Confidentialite

--- lib/profil/consent_text.dart
+++ lib/profil/consent_text.dart
@@ -3,16 +3,16 @@ const String kConsentText = '''
 Politique de confidentialité – Application ASConnect
 
 1) Identité du responsable de traitement
-Les données collectées dans l’application ASConnect sont traitées par FAST France, en qualité de responsable de traitement.
-Pour toute question relative à vos données ou à l’exercice de vos droits, vous pouvez nous contacter à : contact@fastfrance.org.
+Les données collectées dans l’application ASConnect sont traitées par AngelmanAnalytics, en qualité de responsable de traitement.
+Pour toute question relative à vos données ou à l’exercice de vos droits, vous pouvez nous contacter à : contact@angelmananalytics.org.
 ____________________________________________
 2) Finalités des traitements
 Les traitements de données réalisés via l’application poursuivent les finalités suivantes :
 •	Création et gestion de votre compte utilisateur pour un accès nominatif à l’application ASConnect ;
 •	Utilisation des fonctionnalités de géolocalisation afin de visualiser :
-o	des points d’intérêt liés au syndrome d’Angelman,
-o	et/ou des profils de personnes concernées, selon différents critères (distance géographique, génotype, tranche d’âge) ;
-•	Amélioration du service et personnalisation de l’expérience utilisateur, incluant la possibilité pour l’utilisateur de choisir s’il souhaite ou non afficher une photo, son nom ou son prénom.
+o	Des profils de personnes concernées, selon différents critères (distance géographique, génotype, tranche d’âge) ;
+o	Amélioration du service et personnalisation de l’expérience utilisateur, incluant la possibilité pour l’utilisateur.
+•	Extraction de données ne permettant pas d'identifier des personnes à des fins statistiques : nombre de personnes atteintes du syndrome d'Angelman, répartition par âge...
 ____________________________________________
 3) Catégories de données collectées
 Les données suivantes peuvent être collectées :
@@ -20,7 +20,7 @@ Les données suivantes peuvent être collectées :
 o	nom, prénom, adresse e-mail, mot de passe, photo,
 o	question et réponse secrète (en cas d’oubli du mot de passe) ;
 •	Données sensibles :
-o	génotype, localisation du téléphone (géolocalisation).
+o	génotype, localisation.
 La base légale de ces traitements repose sur le consentement explicite et éclairé de l’utilisateur.
 ____________________________________________
 4) Durée de conservation des données
@@ -38,7 +38,7 @@ Conformément au Règlement (UE) 2016/679 (RGPD), les utilisateurs disposent des
 •	Droit d’accès, de rectification et d’effacement (« droit à l’oubli ») — directement via leur profil ou le menu Politique de confidentialité → Droit à l’oubli ;
 •	Droit d’opposition, de portabilité et de limitation du traitement ;
 •	Droit de retrait du consentement à tout moment.
-Pour exercer ces droits (autres que le droit à l’oubli accessible depuis l’application), vous pouvez envoyer un e-mail à contact@fastfrance.org.
+Pour exercer ces droits (autres que le droit à l’oubli accessible depuis l’application), vous pouvez envoyer un e-mail à contact@angelmananalytics.org.
 ____________________________________________
 7) Sécurité et confidentialité
 Toutes les données sont chiffrées lors du stockage et de la transmission.
@@ -46,5 +46,5 @@ Les mots de passe sont hachés selon les bonnes pratiques de sécurité, et tout
 ____________________________________________
 8) Consentement explicite
 L’inscription à l’application ASConnect requiert le consentement explicite et éclairé de l’utilisateur pour les traitements décrits à la section 2.
-L’utilisateur peut retirer ce consentement à tout moment via les paramètres de son compte ou par contact direct à contact@fastfrance.org.
+L’utilisateur peut retirer ce consentement à tout moment via les paramètres de son compte ou par contact direct à contact@angelmananalytics.org.
 ''';
