9ff358784c9d9c0511de833081c3573ed4ade0f0
anthonymoisan <anthonymoisan@yahoo.fr>
2026-02-26 09:42:07 +0100

    Modification consentement

--- lib/profil/consent_text.dart
+++ lib/profil/consent_text.dart
@@ -8,11 +8,11 @@ Pour toute question relative à vos données ou à l’exercice de vos droits, v
 ____________________________________________
 2) Finalités des traitements
 Les traitements de données réalisés via l’application poursuivent les finalités suivantes :
-•	Création et gestion de votre compte utilisateur pour un accès nominatif à l’application ASConnect ;
-•	Utilisation des fonctionnalités de géolocalisation afin de visualiser :
+o	Création et gestion de votre compte utilisateur pour un accès nominatif à l’application ASConnect ;
+o	Utilisation des fonctionnalités de géolocalisation afin de visualiser :
 o	Des profils de personnes concernées, selon différents critères (distance géographique, génotype, tranche d’âge) ;
 o	Amélioration du service et personnalisation de l’expérience utilisateur, incluant la possibilité pour l’utilisateur.
-•	Extraction de données ne permettant pas d'identifier des personnes à des fins statistiques : nombre de personnes atteintes du syndrome d'Angelman, répartition par âge...
+o	Extraction de données ne permettant pas d'identifier des personnes à des fins statistiques : nombre de personnes atteintes du syndrome d'Angelman, répartition par âge...
 ____________________________________________
 3) Catégories de données collectées
 Les données suivantes peuvent être collectées :
@@ -36,9 +36,9 @@ L’application Web est hébergé chez O2Switch en France, consultez https://www
 ____________________________________________
 6) Droits des utilisateurs
 Conformément au Règlement (UE) 2016/679 (RGPD), les utilisateurs disposent des droits suivants :
-•	Droit d’accès, de rectification et d’effacement (« droit à l’oubli ») — directement via leur profil ou le menu Politique de confidentialité → Droit à l’oubli ;
-•	Droit d’opposition, de portabilité et de limitation du traitement ;
-•	Droit de retrait du consentement à tout moment.
+o	Droit d’accès, de rectification et d’effacement (« droit à l’oubli ») — directement via leur profil ou le menu Politique de confidentialité → Droit à l’oubli ;
+o	Droit d’opposition, de portabilité et de limitation du traitement ;
+o	Droit de retrait du consentement à tout moment.
 Pour exercer ces droits (autres que le droit à l’oubli accessible depuis l’application), vous pouvez envoyer un e-mail à contact@angelmananalytics.org.
 ____________________________________________
 7) Sécurité et confidentialité
@@ -48,4 +48,12 @@ ____________________________________________
 8) Consentement explicite
 L’inscription à l’application ASConnect requiert le consentement explicite et éclairé de l’utilisateur pour les traitements décrits à la section 2.
 L’utilisateur peut retirer ce consentement à tout moment via les paramètres de son compte ou par contact direct à contact@angelmananalytics.org.
+____________________________________________
+9) Règles de bon usage sur les messages échangés
+o	Ne publiez pas d'attaques ou de commentaires négatifs concernant des groupes. Tout commentaire visant à harceler, dénigrer, menacer ou abuser d'une personne sera supprimé.
+o	Soyez aimable et courtois. Nous sommes tous impliqués dans la création d'un environnement positif, informatif et respectueux. Les débats sains sont naturels, mais la gentillesse est de rigueur.
+o	Pas de commentaires irrespectueux. L'intimidation sous toutes ses formes n'est pas autorisée et les commentaires dégradants ne sont pas tolérés. Ne publiez pas de propos blasphématoires, diffamatoires, offensants ou violents.
+o	La transmission de conseils ne doient pas être perçue comme des recommandations médicales. 
+o	Pas de publicité. 
+o	Le non respect des règles peut conduire l'administrateur à supprimer un profil.
 ''';
